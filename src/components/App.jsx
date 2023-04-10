0fba55caa37ee0a771a75af1b97b6d4d71146375
AlexFrolov <113615299+ThatGull@users.noreply.github.com>
2023-04-10 19:48:18 +0300

    fb2

---
+++ src/components/App.jsx
@@ -0,0 +1,60 @@
+import { Component } from 'react';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statistics } from './Statistics/Statistics';
+import { Section } from './Section/Section';
+import { Notification } from './Notification/Notification';
+import PropTypes from 'prop-types';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  updateState = feedback => {
+    this.setState(prevState => ({ [feedback]: prevState[feedback] + 1 }));
+  };
+
+  countTotalFeedback() {
+    return this.state.good + this.state.neutral + this.state.bad;
+  }
+
+  countPositiveFeedbackPercentage() {
+    return Math.round((100 / this.countTotalFeedback()) * this.state.good);
+  }
+
+  render() {
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={['good', 'neutral', 'bad']}
+            updateState={this.updateState}
+          />
+        </Section>
+        <Section title="Statistics">
+          {this.countTotalFeedback() !== 0 ? (
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              totalFeedback={this.countTotalFeedback()}
+              feedbackPercentage={this.countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <Notification message="There is no feedback"></Notification>
+          )}
+        </Section>
+      </>
+    );
+  }
+}
+
+App.propTypes = {
+  feedback: PropTypes.string,
+  good: PropTypes.number,
+  neutral: PropTypes.number,
+  bad: PropTypes.number,
+  total: PropTypes.number,
+};
